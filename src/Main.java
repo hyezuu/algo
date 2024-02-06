618d6086f3bbd2db89e95f892b631d31c386b10d
hyezuu <duudu0704@gmail.com>
2024-02-06 20:45:02 +0900

    update practice

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
+        Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
-
-        System.out.print(main.solution(str));
+        System.out.print(T.solution(str));
     }
 
     public String solution(String str) {
-        char[] ch = str.toCharArray();
+        str = str + " ";
         StringBuilder sb = new StringBuilder();
         int count = 1;
-        for(int i=0; i<ch.length-1; i++){
-            if(ch[i]!=ch[i+1]){
-                sb.append((char)ch[i]);
-                sb.append(count);
+
+        for(int i=0; i<str.length()-1;i++){
+            if(str.charAt(i)==str.charAt(i+1)){
+                count++;
+            }
+            else {
+                sb.append(str.charAt(i));
+                if(count>1) sb.append(count);
                 count = 1;
             }
-            else count++;
         }
-        return sb.toString().replaceAll("1","");
+        return sb.toString();
     }
 }
