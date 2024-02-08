5573cbd2a3bc53889fc8ec6c340ed0f008665d5a
hyezuu <duudu0704@gmail.com>
2024-02-08 16:24:55 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,20 +11,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        bw.write(T.solution(n));
+        int[] nums = new int[n];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(stk.nextToken());
+        }
+        bw.write(T.solution(n, nums));
         bw.flush();
     }
-    public String solution(int n){
+    public String solution(int n, int[] nums){
         StringBuilder sb = new StringBuilder();
-        int[] nums = new int[n];
-        nums[0] = 1;
-        nums[1] = 1;
-        for(int i=2; i<n ;i++){
-            nums[i] = nums[i-1]+nums[i-2];
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<n ;i++){
+            int rvs = Integer.parseInt(sb.append(nums[i]).reverse().toString());
+            boolean isPrime = true;
+            for(int j=1; j<=Math.sqrt(rvs);j++){
+                if((j!=1 && rvs%j==0) || rvs==1){
+                    isPrime=false;
+                }
+            }
+            if(isPrime){
+                answer.append(rvs).append(" ");
+            }
+            sb.setLength(0);
         }
-        for(int num : nums){
-            sb.append(num).append(" ");
-        }
-        return sb.substring(0,sb.length()-1);
+        return answer.substring(0,answer.length()-1);
     }
 }
