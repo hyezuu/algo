6be941980df0c171306bec2de3bc3e9e69ceaed5
hyezuu <duudu0704@gmail.com>
2024-02-08 15:31:41 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
