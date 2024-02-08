2c822b8c8eaf198171495c9d365f82d0ccbe9cec
hyezuu <duudu0704@gmail.com>
2024-02-09 00:03:51 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,47 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        Integer[] nums = new Integer[n];
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(stk.nextToken());
+        int n = sc.nextInt();
+        int[][] nums = new int[n + 2][n + 2];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                nums[i][j] = sc.nextInt();
+            }
         }
         bw.write(T.solution(n, nums));
         bw.flush();
+//        System.out.println("");
+//        for(int i=0; i<=n+1; i++){
+//            for(int j=0; j<=n+1; j++){
+//                System.out.print(nums[i][j]+" ");
+//            }
+//            System.out.println("");
+//        }
     }
-    public String solution(int n, Integer[] nums){
-        Integer[] rank = Arrays.copyOf(nums, n);
-        StringBuilder sb = new StringBuilder();
-        Map<Integer, Integer> map = new HashMap<>();
-        Arrays.sort(rank, Comparator.reverseOrder());
-        map.put(rank[0], 1);
-        for(int i=1; i<n ; i++){
-            if(rank[i]==rank[i-1]){
-                map.put(rank[i], map.get(rank[i-1]));
-            }
-            else {
-                map.put(rank[i], i+1);
+
+    public String solution(int n, int[][] nums) {
+        int[] aroundX = {-1, 0, 0, 1};
+        int[] aroundY = {0, 1, -1, 0};
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                boolean isBigger = true;
+                for (int k = 0; k < 4; k++) {
+                    if (nums[i][j] <= nums[i + aroundX[k]][j + aroundY[k]]) {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger) answer++;
+
             }
         }
-        for(int i=0; i<n; i++){
-            sb.append(map.get(nums[i])).append(" ");
-        }
-        return sb.substring(0, sb.length()-1);
+        return answer + "";
     }
 }
