67b22e25c8db8b059fef8c4b92ac424c12774741
hyezuu <duudu0704@gmail.com>
2024-02-10 15:13:53 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        T.solution(n);
-    }
-    public void solution(int n) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a=1, b=1, c;
-        bw.write(a+" "+b+" ");
-        for(int i=2; i<n; i++){
-            c=a+b;
-            bw.write(c+" ");
-            a=b;
-            b=c;
-        }
+        int n = Integer.parseInt(br.readLine());
+        bw.write(T.solution(n));
         bw.flush();
+    }
+    public String solution(int n){
+        int count = 0;
+        int[] ch = new int[n+1];
+        for(int i=2; i<=n; i++){
+            if(ch[i]==0){
+                count++;
+                for(int j=i; j<=n; j=j+i){//j는 i의 배수만큼 증가하니까. i씩 증가!
+                    ch[j] = 1;
+                }
+            }
+        }
+        return count+"";
     }
 }
