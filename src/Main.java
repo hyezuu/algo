a46648c33e04a7e0e82119173467268e8f1a7ed3
hyezuu <duudu0704@gmail.com>
2024-02-10 15:39:19 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,20 +12,37 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        bw.write(T.solution(n));
+        int[] nums = new int[n];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(stk.nextToken());
+        }
+        for(int i : T.solution(n, nums)){
+            bw.write(i+ " ");
+        }
         bw.flush();
     }
-    public String solution(int n){
-        int count = 0;
-        int[] ch = new int[n+1];
-        for(int i=2; i<=n; i++){
-            if(ch[i]==0){
-                count++;
-                for(int j=i; j<=n; j=j+i){//j는 i의 배수만큼 증가하니까. i씩 증가!
-                    ch[j] = 1;
-                }
+    public ArrayList<Integer> solution(int n, int[] nums){
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0; i<n ;i++){
+            int tmp = nums[i];
+            int res = 0;
+            while(tmp>0){
+                int t = tmp%10;
+                res = res*10+t;
+                tmp= tmp/10;
+            }
+            if(isPrime(res)) answer.add(res);
+        }
+        return answer;
+    }
+    public boolean isPrime(int num){
+        if(num == 1) return false;
+        for(int i=2; i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
             }
         }
-        return count+"";
+        return true;
     }
 }
