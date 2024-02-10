f8301aa76137e3cc8998cd72a26f8a0ba738fe93
hyezuu <duudu0704@gmail.com>
2024-02-10 16:43:42 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,39 @@ import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};//시계방향
+    public String solution(int n, int[][] nums) {
+        int answer = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                boolean flag = true;
+                for(int k=0; k<4; k++){
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+
+                    if(nx>=0 && nx<n && ny>=0 && ny<n && nums[nx][ny]>=nums[i][j]){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag) answer++;
+            }
+        }
+        return answer+"";
+    }
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = sc.nextInt();
         int[][] nums = new int[n][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 nums[i][j] = sc.nextInt();
             }
         }
         bw.write(T.solution(n, nums));
         bw.flush();
-    }
-    public String solution(int n, int[][] nums){
-        int answer = Integer.MIN_VALUE;
-        int sum1, sum2;
-        for(int i=0; i<n; i++){
-            sum1=sum2=0;
-            for(int j=0; j<n; j++){
-                sum1+=nums[i][j];//i==행번호, j==열번호 (열 변하고 행 고정 -> 행의 총합)
-                sum2+=nums[j][i];//j==행번호, i==열번호 (행 변하고 열 고정 -> 열의 총합)
-            }
-            answer = Math.max(answer, sum1);
-            answer = Math.max(answer, sum2);
-        }
-        sum1=sum2=0;
-        for(int i=0; i<n; i++){//대각선 합 구하기
-            sum1+=nums[i][i];
-            sum2+=nums[i][n-1-i];
-        }
-        answer = Math.max(answer, sum1);
-        answer = Math.max(answer, sum2);
-        return answer+"";
     }
 }
