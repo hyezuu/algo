9a7e90225706b27c75d06408e09c7100d2aae2a5
hyezuu <duudu0704@gmail.com>
2024-02-10 14:44:21 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,23 +13,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
+        int[] numsA = new int[n];
+        int[] numsB = new int[n];
 
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringTokenizer stkA = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(stk.nextToken());
+            numsA[i] = Integer.parseInt(stkA.nextToken());
         }
-        bw.write(T.solution(n, nums));
+        StringTokenizer stkB = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            numsB[i] = Integer.parseInt(stkB.nextToken());
+        }
+        bw.write(T.solution(n, numsA,numsB));
         bw.flush();
     }
-    public String solution(int n, int[] nums){
-        ArrayList<Integer> answer = new ArrayList<>();
-        answer.add(nums[0]);
-        for(int i=1; i<n; i++){
-            if(nums[i]>nums[i-1]){
-                answer.add(nums[i]);
-            }
+    public String solution(int n, int[] numsA, int[]numsB){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n ;i++){
+            if(numsA[i]==numsB[i]){
+                sb.append('D').append("\n");
+            } else if (numsA[i]==1&&numsB[i]==3) sb.append('A').append("\n");
+             else if (numsA[i]==2&&numsB[i]==1) sb.append('A').append("\n");
+             else if (numsA[i]==3&&numsB[i]==2) sb.append('A').append("\n");
+             else sb.append('B').append("\n");
         }
-
+        return sb.substring(0,sb.length()-1);
     }
 }
