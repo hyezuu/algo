65342746007dd5dccd204df7c9dd226497301769
hyezuu <duudu0704@gmail.com>
2024-02-10 14:25:50 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,36 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Main T = new Main();
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = sc.nextInt();
-        int[][] nums = new int[n + 2][n + 2];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                nums[i][j] = sc.nextInt();
-            }
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(stk.nextToken());
         }
-        bw.write(T.solution(n, nums));
+        for(int x : T.solution(n, nums)){
+            bw.write(x+" ");
+        }
         bw.flush();
-//        System.out.println("");
-//        for(int i=0; i<=n+1; i++){
-//            for(int j=0; j<=n+1; j++){
-//                System.out.print(nums[i][j]+" ");
-//            }
-//            System.out.println("");
-//        }
     }
-
-    public String solution(int n, int[][] nums) {
-        int[] aroundX = {-1, 0, 0, 1};
-        int[] aroundY = {0, 1, -1, 0};
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                boolean isBigger = true;
-                for (int k = 0; k < 4; k++) {
-                    if (nums[i][j] <= nums[i + aroundX[k]][j + aroundY[k]]) {
-                        isBigger = false;
-                        break;
-                    }
-                }
-                if (isBigger) answer++;
-
+    public ArrayList<Integer> solution(int n, int[] nums){
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(nums[0]);
+        for(int i=1; i<n; i++){
+            if(nums[i]>nums[i-1]){
+                answer.add(nums[i]);
             }
         }
-        return answer + "";
+        return answer;
     }
 }
