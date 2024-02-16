7554f0d318ddd98bab1267d9e51a36d1142c762e
hyezuu <duudu0704@gmail.com>
2024-02-17 00:10:00 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,40 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
 
-    public String solution(int n) {
-        int answer=0, cnt=1;//연속된자연수의갯수
-        n--;//n=n-1;
-        while (n>0){
-            cnt++;//cnt==2
-            n-=cnt;//n=n-2;(1,2를뺐음)
-            if(n%cnt==0) answer++;
+    public String solution(int n, int k, int[] arr) {
+        int max = 0, lt = 0, cnt = 0;
+        boolean[] chk = new boolean[arr.length];
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) {
+                cnt++;
+                chk[rt] = true;
+            }
+            while (cnt > k) {
+                if (chk[lt]) cnt--;
+                lt++;
+            }
+            if (cnt <= k) max = Math.max(max, rt - lt + 1);
         }
-        return answer+"";
+        return max + "";
     }
 
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        bw.write(T.solution(n));
+        StringTokenizer con = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(con.nextToken());
+        int k = Integer.parseInt(con.nextToken());
+        StringTokenizer nums = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(nums.nextToken());
+        }
+        bw.write(T.solution(n, k, arr));
         bw.flush();
     }
 }
