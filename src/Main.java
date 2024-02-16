ae1c0f42e9fb9e76267ba5e7c7af01f26bb5007b
hyezuu <duudu0704@gmail.com>
2024-02-17 00:18:49 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,16 @@ import java.util.*;
 public class Main {
 
     public String solution(int n, int k, int[] arr) {
-        int max = 0, lt = 0, cnt = 0;
+        int answer = 0, cnt = 0, lt=0;
         for (int rt = 0; rt < n; rt++) {
-            if (arr[rt] == 0) {
-                cnt++;
-            }
+            if (arr[rt] == 0) cnt++;
             while (cnt > k) {
                 if (arr[lt]==0) cnt--;
                 lt++;
             }
-            max = Math.max(max, rt - lt + 1);
+            answer = Math.max(answer, rt - lt + 1);
         }
-        return max + "";
+        return answer + "";
     }
 
     public static void main(String[] args) throws Exception {
