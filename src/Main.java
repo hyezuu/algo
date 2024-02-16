034738a9c5dc5453cec93d772df088200104b4dd
hyezuu <duudu0704@gmail.com>
2024-02-16 16:57:33 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,53 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public String solution(int n, int k, int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        Set<Integer> set = new HashSet<>();
-        int mem = 0;
+    public int[] solution(int n, int m, int[] arr1, int[] arr2) {
+        int[] answer = new int[n + m];
+        int p1 = 0, p2 = 0;
+        for (int i = 0; i < n + m; i++) {
+            if (p1 < n && p2 < m && arr1[p1] <= arr2[p2]) {
+                answer[i] = arr1[p1];
+                p1++;
+            } else if (p1 < n && p2 < m && arr2[p2] < arr1[p1]) {
+                answer[i] = arr2[p2];
+                p2++;
+            } else {
+                if (p1 >= n) {
+                    answer[i] = arr2[p2];
+                    p2++;
+                } else if (p2 >= n) {
+                    answer[i] = arr1[p1];
+                    p1++;
+                }
 
-        for(int i=0; i<k; i++){
-            if(!set.add(arr[i])){
-                mem = i;
             }
         }
-        sb.append(set.size()).append(" ");
-        for(int i=k-1; i<n; i++){
-            if(i-k+1!=mem) set.remove(arr[i-k+1]);
-            if(!set.add(arr[i])) mem = i;
-            sb.append(set.size()).append(" ");
-        }
-        return sb.toString();
+        return answer;
     }
 
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer cond = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(cond.nextToken());
-        int k = Integer.parseInt(cond.nextToken());
-        StringTokenizer days = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]= Integer.parseInt(days.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer nums1 = new StringTokenizer(br.readLine());
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(nums1.nextToken());
         }
-        bw.write(T.solution(n, k, arr));
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer nums2 = new StringTokenizer(br.readLine());
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = Integer.parseInt(nums2.nextToken());
+        }
+
+        for (int i : T.solution(n, m, arr1, arr2)) {
+            bw.write(i + " ");
+        }
         bw.flush();
     }
 }
