dd3f6da0d12f5d467fcd61c25c9ff773609803aa
hyezuu <duudu0704@gmail.com>
2024-02-17 16:38:51 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,32 @@ import java.util.*;
 
 public class Main {
 
-    public String solution(int n, String str) {
-        char answer =' ';
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char x : str.toCharArray()){
-            map.put(x, map.getOrDefault(x,0)+1);
-        }
-        //System.out.println(map.containsKey('F')); //메서드 연습용
-//        System.out.println("     초기 map 크기 : "+map.size()); //메서드 연습용(사이즈)
-//        System.out.println("    'A' 제거, 밸류 : "+ map.remove('A'));
-//        System.out.println("remove 실행 후 크기 : "+map.size());
+    public String solution(String str1, String str2) {
+        Map<Character,Integer> map1 = new LinkedHashMap<>();
+        Map<Character,Integer> map2 = new LinkedHashMap<>();
 
-        int max = Integer.MIN_VALUE;
-        for(char key : map.keySet()){
-        //System.out.println(key+" "+map.get(key));
-            if(map.get(key)>max){
-                max = map.get(key);
-                answer = key;
+        for(int i=0; i<str1.length(); i++){
+            map1.put(str1.charAt(i),map1.getOrDefault(str1.charAt(i),0)+1);
+            map2.put(str2.charAt(i),map2.getOrDefault(str2.charAt(i),0)+1);
+        }
+        if(map1.size()==map2.size()){
+            for(char key : map1.keySet()){
+                if(!map1.get(key).equals(map2.get(key))){
+                    return "NO";
+                }
             }
         }
-        return answer+"";
+        else return "NO";
+        return "YES";
     }
 
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        bw.write(T.solution(n, str));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        bw.write(T.solution(str1, str2));
         bw.flush();
     }
 }
