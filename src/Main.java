56b0aedb495419f2e8d5ea900a50ee2474a6dd0a
hyezuu <duudu0704@gmail.com>
2024-02-17 22:22:27 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,27 @@ import java.util.*;
 
 public class Main {
 
-    public String solution(String str, String str1) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map = new HashMap<>();
-        char[] arr = str.toCharArray();
+    public String solution(String a, String b) {
+        HashMap<Character, Integer> bm = new HashMap<>();
+        HashMap<Character, Integer> am = new HashMap<>();
+        char[] arr = a.toCharArray();
         int lt =0, cnt=0;
-        for(char c : str1.toCharArray()){
-            map1.put(c ,map1.getOrDefault(c,0)+1);
+        for(char c : b.toCharArray()){
+            bm.put(c ,bm.getOrDefault(c,0)+1);
         }
-        for(int rt =0; rt<str.length(); rt++){
-            map.put(arr[rt],map.getOrDefault(arr[rt],0)+1);
-            if(rt-lt+1==str1.length()){
-                if(map1.equals(map)){
-                    cnt++;
-                }
-                map.put(arr[lt],map.get(arr[lt])-1);
-                if(map.get(arr[lt])==0) map.remove(arr[lt]);
-                lt++;
+        for(int i =0; i<b.length()-1; i++){
+            am.put(arr[i],am.getOrDefault(arr[i],0)+1);
+        }
+        for(int rt = b.length()-1; rt<a.length(); rt++){
+            am.put(arr[rt], am.getOrDefault(arr[rt],0)+1);
+            if(am.equals(bm)){
+                cnt++;
             }
+            if(am.get(arr[lt])>1){
+                am.put(arr[lt], am.get(arr[lt])-1);
+            }
+            else am.remove(arr[lt]);
+            lt++;
         }
         return cnt+"";
     }
@@ -32,9 +35,9 @@ public class Main {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String str = br.readLine();
-        String str1 = br.readLine();
-        bw.write(T.solution(str, str1));
+        String a = br.readLine();
+        String b = br.readLine();
+        bw.write(T.solution(a, b));
         bw.flush();
     }
 }
