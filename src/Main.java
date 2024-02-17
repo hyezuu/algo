0161ed8fdd89ad5784c242e58d0daa02d2d154d2
hyezuu <duudu0704@gmail.com>
2024-02-17 16:34:54 +0900

    update map method practice code

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,14 @@ public class Main {
         for(char x : str.toCharArray()){
             map.put(x, map.getOrDefault(x,0)+1);
         }
+        //System.out.println(map.containsKey('F')); //메서드 연습용
+//        System.out.println("     초기 map 크기 : "+map.size()); //메서드 연습용(사이즈)
+//        System.out.println("    'A' 제거, 밸류 : "+ map.remove('A'));
+//        System.out.println("remove 실행 후 크기 : "+map.size());
+
         int max = Integer.MIN_VALUE;
         for(char key : map.keySet()){
-//            System.out.println(x+" "+map.get(x));
+        //System.out.println(key+" "+map.get(key));
             if(map.get(key)>max){
                 max = map.get(key);
                 answer = key;
