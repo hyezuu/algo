48a62279be48186490cb79c1b0ad09f2118cfef4
hyezuu <duudu0704@gmail.com>
2024-02-19 19:22:31 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,35 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Stack;
 
 //stack -> last in first out
 public class Main {
-    public String solution(int[][] board, int[] moves) {
+    public String solution(String input) {
         Stack<Integer> stack = new Stack<>();
-        int answer = 0;
-        for(int pos : moves){
-            for(int i=0; i<board.length; i++){
-                if(board[i][pos-1]!=0){
-                    int tmp = board[i][pos-1];
-                    board[i][pos-1] = 0;
-                    if(!stack.isEmpty()&&tmp == stack.peek()){
-                        answer+=2;
-                        stack.pop();
-                    }
-                    else stack.push(tmp);
-                    break;
-                }
+        for(char x :input.toCharArray()){ //문자는 기본적으로 정수형타입이니까. 변환해줘야함
+            if(Character.isDigit(x))stack.push(x-'0');//||x-48
+
+            else{
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if (x=='+')stack.push(lt + rt);
+                else if (x=='-')stack.push(lt - rt);
+                else if (x=='*')stack.push(lt * rt);
+                else if (x=='/')stack.push(lt / rt);
             }
         }
-        return answer+"";
+//        return stack.peek()+"";
+        return stack.get(0)+"";
     }
 
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int[][] board = new int[n][n];
-        for(int i=0 ;i<n; i++){
-            StringTokenizer arrs = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
-                board[i][j] = Integer.parseInt(arrs.nextToken());
-            }
-        }
-        int m = Integer.parseInt(br.readLine());
-        StringTokenizer move = new StringTokenizer(br.readLine());
-        int[] moves = new int[m];
-        for(int i=0; i<m; i++){
-            moves[i] = Integer.parseInt(move.nextToken());
-        }
-
-        bw.write(T.solution(board, moves));
+        String input = br.readLine();
+        bw.write(T.solution(input));
         bw.flush();
     }
 }
+//(Character.getNumericValue(x))
