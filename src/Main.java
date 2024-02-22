abf7953c92ac965c40ca1ee54aa287995bc6abef
hyezuu <duudu0704@gmail.com>
2024-02-22 22:50:08 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public String solution(int n ,int[] arr){
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n; i++){
-            for(int j=1; j<n-i; j++){
-                if(arr[j-1]>arr[j]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = tmp;
-                }
+        for(int i=0; i<n-1; i++){
+            int idx = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j]<arr[idx])idx = j;
             }
+            int tmp =arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
         }
         for(int a : arr){
             sb.append(a).append(" ");
@@ -29,10 +28,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer stk = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
-        }
-
+        for(int i=0; i<n; i++)arr[i] = Integer.parseInt(stk.nextToken());
         bw.write(T.solution(n, arr));
         bw.flush();
     }
