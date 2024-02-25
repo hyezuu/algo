016e23d15c4f3027fed0974297a0652d3eb9a6f9
hyezuu <duudu0704@gmail.com>
2024-02-25 22:02:04 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,37 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Point implements Comparable<Point>{
-    int x;
-    int y;
-
-    Point(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public int compareTo(Point o){
-        if(this.x==o.x) return o.y-this.y;
-        else return o.x-this.x;
-    }
-}
 public class Main {
+    public String solution(int[] arr,int n,  int m){
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = arr.length-1;
+        int mid;
+        while (lt<=rt){
+            mid = (lt+rt)/2;
+            if(arr[mid]==m) return mid+1+"";
+            else if(arr[mid] > m) rt=mid-1;
+            //arr[mid] > m 이면 mid 보다 아래인덱스에 m이 존재하는것이다!
+            else lt=mid+1;
+        }
+        return "-1";
+    }
     public static void main(String[] args) throws Exception {
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Point> arr = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            StringTokenizer lines = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(lines.nextToken());
-            int y = Integer.parseInt(lines.nextToken());
-            arr.add(new Point(x,y));
+        StringTokenizer cond = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(cond.nextToken());
+        int m = Integer.parseInt(cond.nextToken());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n; i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
-        Collections.sort(arr);
-
-        for(Point o : arr){
-            bw.write(o.x+" "+o.y+"\n");
-        }
+        bw.write(T.solution(arr,n,m));
         bw.flush();
     }
 }
