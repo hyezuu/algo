8b635b1248eb3bbd6dd23be816db86ba0bd53c00
hyezuu <duudu0704@gmail.com>
2024-02-25 21:36:26 +0900

    update ref

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
-    public String solution(int n, int[] nums){
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] tmp = nums.clone();
-        Arrays.sort(tmp);
-        for(int i=0; i<n; i++){
-            if(nums[i]!=tmp[i]) answer.add(i+1);
-        }
-        for(int x : answer){
-            sb.append(x).append(" ");
-        }
-        return sb.toString();
-    }
-    public static void main(String[] args) throws Exception {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer lines = new StringTokenizer(br.readLine());
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(lines.nextToken());
-        }
+class Point implements Comparable<Point>{
+    int x;
+    int y;
 
-        System.out.print(T.solution(n, nums));
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o){
+        if(this.x==o.x) return this.y-o.y;
+        else return this.x-o.x;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Point> arr = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            StringTokenizer lines = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(lines.nextToken());
+            int y = Integer.parseInt(lines.nextToken());
+            arr.add(new Point(x,y));
+        }
+        Collections.sort(arr);
+
+        for(Point o : arr){
+            bw.write(o.x+" "+o.y+"\n");
+        }
+        bw.flush();
     }
 }
