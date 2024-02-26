8f723006fb1a96ab9ce941ff5a98398c73caec20
hyezuu <duudu0704@gmail.com>
2024-02-26 21:49:45 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,42 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public String solution(int[] arr,int n,  int m){
-        Arrays.sort(arr);
-        String answer = "";
-        int lt = 0, rt = n-1;
-        while (lt<=rt){
-            int mid = (lt+rt)/2;
-            if(arr[mid]==m) {
-                answer = mid+1+"";
-                break;
-            }
-            else if(arr[mid] > m) rt=mid-1;
-            //arr[mid] > m 이면 mid 보다 아래인덱스에 m이 존재하는것이다!
-            else lt=mid+1;
-        }
-        return answer;
-    }
     public static void main(String[] args) throws Exception {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer cond = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(cond.nextToken());
-        int m = Integer.parseInt(cond.nextToken());
         StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+        StringTokenizer nums = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        for(int i=0;i<n; i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
+        int lt = 0, rt = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(nums.nextToken());
+            if(lt<arr[i])lt=arr[i];
+            rt+=arr[i];
         }
-        bw.write(T.solution(arr,n,m));
+        int answer = Integer.MAX_VALUE;
+        while (lt<=rt){
+            int mid = (lt+rt)/2;
+            int sum = 0,i=0,cnt=1;
+            while (i<arr.length){
+                sum+=arr[i];
+                if(sum>mid){
+                    sum=arr[i];
+                    cnt++;
+                }
+                i++;
+            }
+            if(cnt<=m) {
+                rt=mid-1;
+                answer = Math.min(mid,answer);
+            }
+            else lt=mid+1;
+        }
+        bw.write(answer+"");
         bw.flush();
     }
 }
