3cc9715e6a5bc2a9ceb446e8042254db095596f6
hyezuu <duudu0704@gmail.com>
2024-02-28 18:30:22 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,41 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
-    public int count(int[] arr, int capacity){
-        int cnt = 1, sum=0;
-        Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++){
-            if(sum+capacity>=arr[i]){
-                cnt++;
-            }
-            else sum=arr[i];
+    public String solution (String str, ArrayList<String> list){
+        int lt = list.size();
+        ArrayList<Character> answer = new ArrayList<>();
+        for(char x : str.toCharArray()){
+            answer.add(x);
         }
-        return cnt;
-    }
-    public int solution(int n, int m, int[] arr){
-        int answer = 0;
-        int min = Arrays.stream(arr).min().getAsInt();
-        int max = Arrays.stream(arr).max().getAsInt();
-        int lt = 1;
-        int rt = max-min;
+        for(String s : list){
+            char c = s.charAt(0);
+            if(lt>0) {
+                if(c=='L') lt--;
+                else if(c=='B') answer.remove(lt);
+            }
+            else if (lt<answer.size()) {
+                if(c=='D') lt++;
+            }
+            else if (c=='P') answer.add(lt, s.charAt(2));
+        }
 
-        while (lt<=rt){
-            int mid = (lt+rt)/2;
-            if(count(arr, mid)>=m){
-                answer = mid;
-                rt = mid-1;
-            }
-            else lt = mid+1;
-        }
-        return answer;
+        return String.valueOf(answer);
     }
     public static void main(String[] args) throws Exception {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
-        StringTokenizer nums = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        int lt = 0, rt = 0;
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(nums.nextToken());
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(br.readLine());
         }
-        bw.write(T.solution(n, m , arr)+"");
+
+        bw.write(T.solution(str, list));
         bw.flush();
     }
 }
