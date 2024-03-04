c8327fb194389dfcb42f3055efe4cb6fcdacea7a
hyezuu <duudu0704@gmail.com>
2024-03-04 13:13:08 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,53 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    public boolean minD(int[] arr, int capacity, int c){
+        int cnt = 0, sum= arr[0];
+        Arrays.sort(arr);
+        for(int i=1; i<arr.length; i++){
+            if(sum+capacity>=arr[i]){
+                cnt++;
+                sum = arr[i];
+            }
+
+            if(cnt>=c)return true;
+        }
+        return false;
+    }
+    public int solution(int n, int c, int[] arr){
+        int answer = 0;
+        int min = Arrays.stream(arr).min().getAsInt();
+        int max = Arrays.stream(arr).max().getAsInt();
+        int lt = 1;
+        int rt = max-min;
+
+        while (lt<=rt){
+            int mid = (lt+rt)/2;
+            if(minD(arr, mid, c)){
+                answer = mid;
+                rt = mid-1;
+            }
+            else lt = mid+1;
+        }
+        return answer;
+    }
     public static void main(String[] args) throws Exception {
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder answer = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            Stack<Character> stackL = new Stack<>();
-            Stack<Character> stackR = new Stack<>();
-            for (char c : str.toCharArray()) {
-                if (Character.isDigit(c) || Character.isAlphabetic(c)) {
-                    stackL.push(c);
-                }
-                if (!stackL.isEmpty() && c == '<') stackR.push(stackL.pop());
-                else if (!stackR.isEmpty() && c == '>') stackL.push(stackR.pop());
-                else if (!stackL.isEmpty() && c == '-') stackL.pop();
-            }
-            while (!stackR.isEmpty()){
-                stackL.push(stackR.pop());
-            }
-            for(char c : stackL){
-                answer.append(c);
-            }
-            answer.append("\n");
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+        StringTokenizer nums = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(nums.nextToken());
         }
-        bw.write(answer.substring(0,answer.length()));
+        bw.write(T.solution(n, m , arr)+"");
         bw.flush();
     }
 }
