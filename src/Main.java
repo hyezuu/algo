4b6ddd43947c8dee2fd334743498b6a98ba3b773
hyezuu <duudu0704@gmail.com>
2024-03-14 01:42:19 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -6,59 +6,49 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
-이진트리 순회(넓이우선탐색 : 레벨탐색)
-루트에서 특정 지점으로 갈 때의 최단거리
-queue를 사용한다 !(dfs는 스택)
-        1           0레벨
- (왼)2     (오)3     1레벨(루트에서 한번만에 갈 수 있는 노드들)
- 4    5    6   7    2레벨(루트에서 두번만에 갈 수 있는 노드들)
+상태트리
+입력 5, 14
+5 14
 
-레벨 탐색 순회 출력 : 1 2 3 4 5 6 7
+0 : 5
+1 : 6 10 4
+2 : 7 11 15
+
+answer : 3
  */
 public class Main {
-    static int E;
-    static boolean[] chk;
-
-    public void BFS(int root, int L) {
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(root);
-        chk[root] = true;
-        while (!Q.isEmpty()) {
+    int[] dis = {1,-1,5};
+    boolean[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+    public int BFS(int s, int e) {
+        ch = new boolean[10001];
+        ch[s] = true;
+        Q.offer(s);
+        int L=0;
+        while(!Q.isEmpty()){
             int len = Q.size();
-            for (int i = 0; i < len; i++) {
-                int tmp = Q.poll();
-
-                if(tmp+1==E||tmp+5==E||tmp-1==E) {
-                    System.out.print(L+1);
-                    return;
-                }
-                else {
-                    if (!chk[tmp + 1]) {
-                        Q.offer(tmp + 1);
-                        chk[tmp + 1] = true;
-                    }
-                    if (!chk[tmp + 5]) {
-                        Q.offer(tmp + 5);
-                        chk[tmp + 5] = true;
-                    }
-                    if (tmp-1>0&&!chk[tmp - 1]) {
-                        Q.offer(tmp - 1);
-                        chk[tmp - 1] = true;
+            for(int i=0;i<len;i++){
+                int x = Q.poll();
+                if(x==e) return L;
+                for(int j=0; j<3; j++){
+                    int nx = x+dis[j];
+                    if(nx>=1 && nx<=10000 && !ch[nx]){
+                        ch[nx]= true;
+                        Q.offer(nx);
                     }
                 }
-
             }
             L++;
         }
+        return 0;
     }
 
     public static void main(String[] args) throws IOException {
-        Main tree = new Main();
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(stk.nextToken());
-        E = Integer.parseInt(stk.nextToken());
-        chk = new boolean[20000];
-        tree.BFS(S, 0);
+        int s = Integer.parseInt(stk.nextToken());
+        int e = Integer.parseInt(stk.nextToken());
+        System.out.print(T.BFS(s,e));
     }
 }
