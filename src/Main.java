7e1e24dcdc779acac2b1515cbc46a856eb444bb2
hyezuu <duudu0704@gmail.com>
2024-05-06 21:08:47 +0900

    update solution

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,23 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution {
-    public int solution(String s) {
-        int answer = 0;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        char c = br.readLine().charAt(0);
+        System.out.println(T.solution(str, c));
+    }
+    public int solution (String str , char c){
+        str = str.toLowerCase();
+        c = Character.toLowerCase(c);
+
         int cnt = 0;
-        Map<Character, Character> map = new HashMap<>();
-
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
-
-
-        for (char a : s.toCharArray()) {
-            if (a == '(' || a == '{' || a == '[') cnt++;
+        for(char a : str.toCharArray()){
+            if(a==c) cnt++;
         }
-        if (s.length() != cnt * 2) return 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char tmp = s.charAt(i);
-            Stack<Character> stack = new Stack<>();
-            boolean chk = true;
-            for (char a : s.toCharArray()) {
-                if (a == '(' || a == '{' || a == '[') stack.push(a);
-                else if (!stack.isEmpty() && map.get(stack.peek()) == a) {
-                    stack.pop();
-                }
-                else {
-                    chk = false;
-                    break;
-                }
-            }
-            if (chk) answer++;
-            s = s.substring(i + 1) + tmp;
-        }
-        return answer;
-
+        return cnt;
     }
 }
