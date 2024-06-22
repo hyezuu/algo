fc5d327b5a17eac7b2594d32f16f6df7540f9d76
hyezuu <duudu0704@gmail.com>
2024-06-22 20:25:45 +0900

    update dp

--- src/Main.java
+++ src/Main.java
@@ -3,86 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class Main {
     public static void main(String[] args) throws IOException {
+
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char[][] grid = new char[n][n];
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            String tmp = br.readLine();
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = tmp.charAt(j);
-            }
+        int[] arr = new int[t];
+
+        for (int i = 0; i < t; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.print(main.solution(n, grid));
+        System.out.print(main.solution(arr));
         br.close();
     }
-
-    public String solution(int n, char[][] graph) {
-        boolean[][] visited = new boolean[n][n];
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        visited[0][0] = true;
-        int cnt = 1;
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
+//dp풀이 2
+    public String solution(int[] arr) {
         StringBuilder sb = new StringBuilder();
-
-        boolean flag = false;
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            char cur = graph[tmp[0]][tmp[1]];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = tmp[0] + dx[i];
-                int ny = tmp[1] + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (visited[nx][ny]) continue;
-                if (graph[nx][ny] == cur) {
-                    q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                } else if (flag) {
-                    if ((cur == 'G' && graph[nx][ny] == 'R') || (cur == 'R' && graph[nx][ny] == 'G')) {
-                        q.offer(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                    }
-                }
-
+        int[] dp = new int[12];
+        for(int n : arr){
+            dp[1] = 1; dp[2] = 2; dp[3] = 4;
+            for(int i=4; i<=n; i++){
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
             }
-            if (q.isEmpty()) {
-                outer : for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (!visited[i][j]) {
-                            q.offer(new int[]{i, j});
-                            visited[i][j] = true;
-                            cnt++;
-                            break outer;
-                        }
-                    }
-//                    int nx = tmp[0] + dx[i];
-//                    int ny = tmp[1] + dy[i];
-//
-//                    if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-//                    if (visited[nx][ny]) continue;
-//                    q.offer(new int[]{nx, ny});
-//                    cnt++;
-//                    visited[nx][ny] = true;
-                }
-            }
-            if (q.isEmpty() && !flag) {
-                flag = true;
-                q.offer(new int[]{0, 0});
-                visited = new boolean[n][n];
-                visited[0][0] = true;
-                sb.append(cnt).append(" ");
-                cnt = 1;
-            }
+            sb.append(dp[n]).append("\n");
         }
-        return sb.append(cnt).toString();
+        return sb.toString();
     }
+//dp풀이 1
+//    public String solution(int[] arr) {
+//        StringBuilder sb = new StringBuilder();
+//        for(int n : arr){
+//            int[] dp = new int[n+1];
+//            dp[0] = 1;
+//            //dp[i] 는 i를 1,2,3의 합으로 나타내는 경우의 수
+//            //dp[4] = dp[1]+dp[2]+dp[3];
+//            for(int i=1; i<=n; i++){
+//                dp[i] += dp[i-1]; //1을 1,2,3으로 나타내는 경우의 수
+//                if(i>=2) dp[i] += dp[i-2]; //2를 1,2,3의 합으로 나타내는 경우의 수
+//                if(i>=3) dp[i] += dp[i-3]; //3을 1,2,3의 합으로 나타내는 경우의 수
+//            }
+//            sb.append(dp[n]).append("\n");
+//        }
+//        return sb.toString();
+//    }
+//ㅇ이건무슨 풀이??
+//    public String solution(int[] nums) {
+//        Stack<Integer> stack = new Stack<>();
+//        StringBuilder sb = new StringBuilder();
+//        int[] arr = {1, 2, 3};
+//
+//        for (int n : nums) {
+//            int cnt = 0;
+//            stack.push(1);
+//            stack.push(2);
+//            stack.push(3);
+//
+//            while (!stack.isEmpty()) {
+//                int tmp = stack.pop();
+//                if (tmp > n) continue;
+//                if (tmp == n) {
+//                    cnt++;
+//                    continue;
+//                }
+//
+//                for (int a : arr) {
+//                    stack.push(tmp + a);
+//                }
+//            }
+//            sb.append(cnt).append("\n");
+//        }
+//        return sb.toString();
+//    }
 }
