ce688e79e92f478ee3848c22a76b5fa82d1e0a00
hyezuu <duudu0704@gmail.com>
2024-08-09 20:40:10 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] interval = new int[4];
+        int[][] board = new int[N][N];
 
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[] students = new int[n];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                students[j] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
-            bw.write(bfs(students) + "\n");
         }
 
-        br.close();
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<4; j++){
+                interval[j] = Integer.parseInt(st.nextToken());
+            }
+            bw.write(prefixSum(interval, board) + "\n");
+        }
+
         bw.flush();
         bw.close();
+        br.close();
     }
-
-    //방문체크는 팀원을 구했거나, 팀원을 구하지 못할경우에 한다
-    //최근에 입력된 값들은 변수에 저장한다.
-    private static int bfs(int[] students) {
-        boolean[] chk = new boolean[students.length];
-        int cnt = 0;
-        for (int i = 0; i < students.length; i++) {
-            Deque<Integer> deque = new ArrayDeque<>();
-            boolean[] visited = new boolean[students.length];
-            if(!chk[i]){
-                deque.add(i);
-                visited[i] = true;
-            }
-            //가장 큐에 들어간 값
-            while (!deque.isEmpty()) {
-                int last = deque.getLast();
-                int next = students[last] - 1;
-                //자기 혼자 팀하고싶을때
-                if(last == next && chk[next]){
-                    chk[next] = true;
-                    break;
-                }
-                if(visited[next] && deque.peekFirst()==next){
-                    while(!deque.isEmpty()){
-                        chk[deque.removeFirst()] = true;
-                    }
-                }
-                if(chk[next] || visited[next]) break;
-
-                deque.add(next);
-                visited[next] = true;
+    //interval = {x1, y1, x2, y2}
+    static int prefixSum(int[] interval, int[][] board) {
+        int sum = 0;
+        for(int x =interval[0]-1; x<=interval[2]-1; x++){
+            for(int y = interval[1]-1; y<=interval[3]-1; y++){
+                sum+=board[x][y];
             }
         }
-        for(boolean b : chk) if(!b) cnt++;
-
-        return cnt;
+        return sum;
     }
 }
+
+//static int[] counts= {0, 0, 0};
+//    static int[][] paper;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        paper = new int[N][N];
+//
+//        for(int i = 0; i < N; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            for(int j = 0; j < N; j++) {
+//                paper[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//        recurse(0, N);
+//
+//        for(int i=0; i<3; i++){
+//            System.out.println(counts[i]);
+//        }
+//    }
+//
+//    static void recurse(int start ,int end) {
+//        int len = end-start;
+//        for(int i=start; i<len; i++){
+//            for(int j=start; j<len; j++){
+//                if(paper[i][j]!=paper[start][start]){
+//                    recurse(,j);
+//                }
+//            }
+//        }
+//
+//        counts[paper[start][start]+1]++;
+//    }
---
+++ src/boj12865/Main2.java
@@ -0,0 +1,23 @@
+package boj12865;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        System.out.println(solution(X));
+    }
+
+    static String solution (int x) {
+        int i= 1;
+        int j = 1;
+
+
+
+        return i+"/"+j;
+    }
+}
