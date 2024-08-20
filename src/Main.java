9def7eec2b41b4e8ca1fd166bd2b7f5f7cfe1c33
hyezuu <duudu0704@gmail.com>
2024-08-20 22:11:24 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,73 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
-
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] interval = new int[4];
-        int[][] board = new int[N][N];
 
-        for(int i=0; i<N; i++){
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i <=N; i++) {
+            graph.add(new LinkedList<>());
+        }
+
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                board[i][j] = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+        int min = Integer.MAX_VALUE;
+        int answer = 0;
+
+        for(int i = 1; i <= N; i++) {
+            if(min > solution(i)){
+                min = solution(i);
+                answer = i;
             }
         }
 
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<4; j++){
-                interval[j] = Integer.parseInt(st.nextToken());
-            }
-            bw.write(prefixSum(interval, board) + "\n");
-        }
+        System.out.print(answer);
 
-        bw.flush();
-        bw.close();
         br.close();
     }
-    //interval = {x1, y1, x2, y2}
-    static int prefixSum(int[] interval, int[][] board) {
+
+    static int solution(int num) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(num);
+
+        boolean[] visited = new boolean[graph.size()];
+        int chk = N-1;
+        int cnt = 0;
+        visited[num] = true;
         int sum = 0;
-        for(int x =interval[0]-1; x<=interval[2]-1; x++){
-            for(int y = interval[1]-1; y<=interval[3]-1; y++){
-                sum+=board[x][y];
+
+        while(!q.isEmpty() && chk > 0) {
+            cnt ++;
+            int cur = q.poll();
+
+            for(int i : graph.get(cur)) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    chk--;
+                    q.offer(i);
+                    sum+= cnt;
+                }
             }
         }
         return sum;
     }
 }
-
-//static int[] counts= {0, 0, 0};
-//    static int[][] paper;
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int N = Integer.parseInt(br.readLine());
-//        paper = new int[N][N];
-//
-//        for(int i = 0; i < N; i++) {
-//            StringTokenizer st = new StringTokenizer(br.readLine());
-//            for(int j = 0; j < N; j++) {
-//                paper[i][j] = Integer.parseInt(st.nextToken());
-//            }
-//        }
-//        recurse(0, N);
-//
-//        for(int i=0; i<3; i++){
-//            System.out.println(counts[i]);
-//        }
-//    }
-//
-//    static void recurse(int start ,int end) {
-//        int len = end-start;
-//        for(int i=start; i<len; i++){
-//            for(int j=start; j<len; j++){
-//                if(paper[i][j]!=paper[start][start]){
-//                    recurse(,j);
-//                }
-//            }
-//        }
-//
-//        counts[paper[start][start]+1]++;
-//    }
